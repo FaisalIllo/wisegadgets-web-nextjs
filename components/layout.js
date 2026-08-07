f3a249eb1310df5946a25bb9585a80510201df96
Faisal Illo <faisalillo@yahoo.com>
2026-08-07 15:10:57 +0800

    Improve GIRIXCO search metadata

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { DefaultSeo } from 'next-seo'
+import { DefaultSeo, OrganizationJsonLd } from 'next-seo'
 
-import { defaultSeo } from 'next-seo.config'
+import { defaultSeo, defaultUrl, logoUrl } from 'next-seo.config'
 import Footer from '@/components/footer'
 import Header from '@/components/header'
 
@@ -9,6 +9,14 @@ function Layout({ children, footer, navigation }) {
   return (
     <React.Fragment>
       <DefaultSeo {...defaultSeo} />
+      <OrganizationJsonLd
+        type="Organization"
+        id={`${defaultUrl}/#organization`}
+        name="GIRIXCO"
+        legalName="GIRIXCO"
+        url={defaultUrl}
+        logo={logoUrl}
+      />
       <Header {...navigation} />
       <div className="max-w-7xl min-h-screen mx-auto mt-10 px-4 sm:px-6">{children}</div>
       <Footer {...footer} navigationPages={navigation.pages} />
